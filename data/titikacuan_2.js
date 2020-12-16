81751c0a7da4cd0963b8ce3293751a8750e8ae2e
IsabellaV01 <76053149+IsabellaV01@users.noreply.github.com>
2020-12-16 07:11:34 +0700

    Add files via upload

---
+++ data/titikacuan_2.js
@@ -0,0 +1,8 @@
+var json_titikacuan_2 = {
+"type": "FeatureCollection",
+"name": "titikacuan_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.408675347218363, -7.13289780311066 ] } }
+]
+}
